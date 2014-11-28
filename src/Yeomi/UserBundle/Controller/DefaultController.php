497e832df51144b1c8b0389cf596cf6fd4478eeb
Loïc Mangold <loic@studio.gd>
2014-11-28 15:38:16 +0100

    m

--- src/Yeomi/UserBundle/Controller/DefaultController.php
+++ src/Yeomi/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+		//Doc333
         return $this->render('YeomiUserBundle:Default:index.html.twig', array('name' => $name));
     }
 }
